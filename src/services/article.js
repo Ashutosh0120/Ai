b2e102c9b090f4c9ff3c085f6c68ab68fce9cf14
Ashutosh Kumar Singh <ashutoshkumarsi@hcl.com>
2023-05-16 14:01:28 +0530

    Upload files

---
+++ src/services/article.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'fc427177e6mshbfa0c66562694d7p1eb8dcjsnec031bf2b330');
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
+
+export const { useLazyGetSummaryQuery } = articleApi
